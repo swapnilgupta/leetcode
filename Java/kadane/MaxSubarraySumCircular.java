e44871f6c0dcaf291881d578b076267b3fc3a31c
Swapnil Gupta <mr.swapnilgupta@gmail.com>
2023-10-27 08:52:37 +0530

    Added New Problems
    MaxSubArraySum
    SearchInRotatedSortedArray
    LRUCache

---
+++ Java/kadane/MaxSubarraySumCircular.java
@@ -0,0 +1,49 @@
+package kadane;
+
+public class MaxSubarraySumCircular {
+
+    public int maxSubarraySumCircular(int[] nums) {
+        int n = nums.length;
+        int maxSum = nums[0];
+        int curMax = nums[0];
+        int minSum = nums[0];
+        int curMin = nums[0];
+        int total = nums[0];
+        for (int i = 1; i < n; i++) {
+            if(curMax > 0) {
+                curMax += nums[i];
+            } else {
+                curMax = nums[i];
+            }
+            maxSum = Math.max(maxSum, curMax); // updating the global max
+            if(curMin < 0) {
+                curMin += nums[i];
+            } else {
+                curMin = nums[i];
+            }
+            minSum = Math.min(minSum, curMin);  // updating the global min
+            total += nums[i]; // calculating the total sum
+        }
+        if (maxSum > 0) { // if the maxSum is greater than 0, then we can return the max of the two cases
+            return Math.max(maxSum, total - minSum); // returning the max of the two cases
+        }
+        return maxSum;
+    }
+
+    // driver code for the above function
+    public static void main(String[] args) {
+        MaxSubarraySumCircular maxSumCircular = new MaxSubarraySumCircular();
+        int[] nums = {1, -2, 3, -2};
+        int result = maxSumCircular.maxSubarraySumCircular(nums);
+        System.out.println(result);
+
+        nums = new int[]{5, -3, 5};
+        result = maxSumCircular.maxSubarraySumCircular(nums);
+        System.out.println(result);
+
+        nums = new int[]{3, -1, 2, -1};
+        result = maxSumCircular.maxSubarraySumCircular(nums);
+        System.out.println(result);
+    }
+
+}
---
+++ Java/linkedlist/LRUCache.java
@@ -0,0 +1,109 @@
+package linkedlist;
+import java.util.*;
+
+/*
+  Your LRUCache object will be instantiated and called as such: LRUCache obj = new
+  LRUCache(capacity); int param_1 = obj.get(key); obj.put(key,value);
+ */
+class LRUCache {
+
+    public static class DoublyLinkedListNode {
+
+        int key;
+        int val;
+        DoublyLinkedListNode prev;
+        DoublyLinkedListNode next;
+
+        DoublyLinkedListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    int cap;
+    Map<Integer, DoublyLinkedListNode> cache;
+    DoublyLinkedListNode head = new DoublyLinkedListNode(-1, -1); // head.next is the first node
+    DoublyLinkedListNode tail = new DoublyLinkedListNode(-1, -1); // tail.prev is the last node
+    // head and tail are the senitals for the LinkedList
+
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+        cache = new HashMap<>();
+        head.next = tail; // Initially, both senitals pointing to each other
+        tail.prev = head;
+    }
+
+    /*
+    Adds a node to this list at the first
+     */
+    public void addNode(DoublyLinkedListNode node) {
+        DoublyLinkedListNode temp = head.next;
+
+        node.prev = head;
+        node.next = head.next;
+
+        head.next = node;
+        temp.prev = node;
+    }
+
+    public void deleteNode(DoublyLinkedListNode node) {
+        DoublyLinkedListNode prev = node.prev;
+        DoublyLinkedListNode next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            DoublyLinkedListNode resNode = cache.get(key);
+            int ans = resNode.val;
+
+            cache.remove(key);
+            deleteNode(resNode);
+            addNode(resNode);
+
+            cache.put(key, resNode);
+            return ans;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            DoublyLinkedListNode curr = cache.get(key);
+            cache.remove(key);
+            deleteNode(curr);
+        }
+        // check for overflow of cache
+        if (cache.size() == cap) {
+            cache.remove(tail.prev.key);
+            deleteNode(tail.prev);
+        }
+        addNode(new DoublyLinkedListNode(key, value));
+        cache.put(key, head.next);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(2);
+
+        // corresponds to ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+        lruCache.put(1, 1);
+        lruCache.put(2, 2);
+
+        System.out.println(lruCache.get(1));  // prints: 1
+
+        lruCache.put(3, 3);
+
+        System.out.println(lruCache.get(2));  // prints: -1
+
+        lruCache.put(4, 4);
+
+        System.out.println(lruCache.get(1));  // prints: -1
+        System.out.println(lruCache.get(3));  // prints: 3
+        System.out.println(lruCache.get(4));  // prints: 4
+    }
+}
+
