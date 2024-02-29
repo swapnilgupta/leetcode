400461b702cf63d66fd4cea343df52a7a307310f
Swapnil Gupta <mr.swapnilgupta@gmail.com>
2024-02-29 11:50:54 +0530

    Update

---
+++ Java/dp/MarkIndicesI.java
@@ -0,0 +1,52 @@
+package dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MarkIndicesI {
+	// nums[2,2,0] and changeIndices[2,2,2,2,3,2,,2,1]
+	public static void main(String[] args) {
+		MarkIndicesI mi = new MarkIndicesI();
+		int[] nums = {2, 2, 0};
+		int[] changeIndices = {2, 2, 2, 2, 3, 2, 2, 1};
+		System.out.println(mi.earliestSecondToMarkIndices(nums, changeIndices));
+	}
+
+	private boolean canMarkIndices(int[] nums, int[] changeIndices, int idx) {
+		Map<Integer, Integer> last = new HashMap<>();
+		for (int i = 0; i < idx; i++) {
+			last.put(changeIndices[i], i);
+		}
+
+		if (last.size() != nums.length) {
+			return false;
+		}
+		int cnt = 0; // record how many we can reduce the number
+		for (int i = 0; i < idx; ++i) {
+			// if it sis last time we visited this index, we must mark this index
+			// so, we check weather this idx already reduced to zero, if not then return false
+			if (i == last.get(changeIndices[i])) {
+				if (cnt < nums[changeIndices[i] - 1]) return false;
+				else cnt -= nums[changeIndices[i] - 1];
+			} else {
+				++cnt;
+			}
+		}
+		return true;
+	}
+
+	public int earliestSecondToMarkIndices(int[] nums, int[] changeIndices) {
+		int n = nums.length;
+		int m = changeIndices.length;
+		int lo = 0, hi = m + 1;
+		while (lo < hi) {
+			int mid = lo + (hi - lo) / 2;
+			if (canMarkIndices(nums, changeIndices, mid)) {
+				hi = mid;
+			} else {
+				lo = mid + 1;
+			}
+		}
+		return hi == m + 1 ? -1 : hi;
+	}
+}
---
+++ Java/math/LargestSquareArea.java
@@ -0,0 +1,64 @@
+package math;
+
+public class LargestSquareArea {
+	// bottomLeft = [[1,1],[2,2],[3,1]], topRight = [[3,3],[4,4],[6,6]]
+	public static void main(String[] args) {
+		LargestSquareArea lsa = new LargestSquareArea();
+		int[][] bottomLeft = {{1, 1}, {2, 2}, {3, 1}};
+		int[][] topRight = {{3, 3}, {4, 4}, {6, 6}};
+		System.out.println(lsa.largestSquareArea(bottomLeft, topRight));
+	}
+
+	private boolean isRectangleOverlap(Rectangle r1, Rectangle r2) {
+		// if one rectangle is on the left side of the other
+		if (r1.topRightX <= r2.bottomLeftX || r2.topRightX <= r1.bottomLeftX) {
+			return false;
+		}
+		// if one rectangle is above the other
+		if (r1.topRightY <= r2.bottomLeftY || r2.topRightY <= r1.bottomLeftY) {
+			return false;
+		}
+		return true;
+	}
+
+	// find the min side of the rectangle formed by the overlapping rectangles
+	private int minSide(Rectangle r1, Rectangle r2) {
+		int x1 = Math.max(r1.bottomLeftX, r2.bottomLeftX);
+		int y1 = Math.max(r1.bottomLeftY, r2.bottomLeftY);
+		int x2 = Math.min(r1.topRightX, r2.topRightX);
+		int y2 = Math.min(r1.topRightY, r2.topRightY);
+		return Math.min(x2 - x1, y2 - y1);
+	}
+
+	public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+		int n = bottomLeft.length;
+		Rectangle[] rectangles = new Rectangle[n];
+		for (int i = 0; i < n; i++) {
+			rectangles[i] = new Rectangle(bottomLeft[i][0], bottomLeft[i][1], topRight[i][0], topRight[i][1]);
+		}
+		long maxArea = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (isRectangleOverlap(rectangles[i], rectangles[j])) {
+					int minSide = minSide(rectangles[i], rectangles[j]);
+					maxArea = Math.max(maxArea, (long) minSide * minSide);
+				}
+			}
+		}
+		return maxArea;
+	}
+
+	static class Rectangle {
+		int bottomLeftX;
+		int bottomLeftY;
+		int topRightX;
+		int topRightY;
+
+		Rectangle(int bottomLeftX, int bottomLeftY, int topRightX, int topRightY) {
+			this.bottomLeftX = bottomLeftX;
+			this.bottomLeftY = bottomLeftY;
+			this.topRightX = topRightX;
+			this.topRightY = topRightY;
+		}
+	}
+}
