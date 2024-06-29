07da19c568b82ac635cc09c4b90d624c51c85f90
Swapnil Gupta <mr.swapnilgupta@gmail.com>
2024-06-29 23:07:59 +0530

    update

--- Java/graph/PathWithMaximumProbability.java
+++ Java/graph/PathWithMaximumProbability.java
@@ -1,2 +1,62 @@
-package graph;public class PathWithMaximumProbability {
+package graph;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PathWithMaximumProbability {
+	public static class Pair {
+		int v;
+		Double d;
+
+		Pair(int v, Double d) {
+			this.v = v;
+			this.d = d;
+		}
+	}
+
+	public double maxProbability(int n, int[][] edges, double[] successorProb, int sn, int en) {
+
+		double[][] adj = new double[n][n];
+
+		for (int i = 0; i < edges.length; i++) {
+
+			adj[edges[i][0]][edges[i][1]] = successorProb[i];
+			adj[edges[i][1]][edges[i][0]] = successorProb[i];
+		}
+
+		boolean[] visited = new boolean[n];
+
+		double[] dist = new double[n];
+
+
+		PriorityQueue<Pair> queue = new PriorityQueue<>((p1, p2) -> Double.compare(p2.d, p1.d));
+
+		queue.add(new Pair(sn, 1.0));
+
+		while (!queue.isEmpty()) {
+			Pair p = queue.poll();
+
+			if (visited[p.v])
+				continue;
+
+			visited[p.v] = true;
+
+			if (p.d >= dist[p.v])
+				dist[p.v] = p.d;
+			else
+				continue;
+
+			if (p.v == en)
+				return dist[p.v];
+
+			for (int i = 0; i < n; i++) {
+				if (adj[p.v][i] != 0.0) {
+					Pair pair = new Pair(i, adj[p.v][i] * p.d);
+					queue.add(pair);
+				}
+			}
+		}
+
+		return dist[en];
+	}
 }
--- Java/greedy/ShoppingOptimization.java
+++ Java/greedy/ShoppingOptimization.java
@@ -5,17 +5,14 @@ import java.util.*;
 public class ShoppingOptimization {
 
 	public static void main(String[] args) {
-		// Define the stores and the items they offer
 		Map<String, Set<String>> stores = new HashMap<>();
 		stores.put("Store A", new HashSet<>(Arrays.asList("Item 1", "Item 2")));
 		stores.put("Store B", new HashSet<>(Arrays.asList("Item 2", "Item 3")));
 		stores.put("Store C", new HashSet<>(Arrays.asList("Item 1", "Item 3", "Item 4")));
 		stores.put("Store D", new HashSet<>(Arrays.asList("Item 4", "Item 5")));
 
-		// Define the required items
 		Set<String> requiredItems = new HashSet<>(Arrays.asList("Item 1", "Item 2", "Item 3", "Item 4"));
 
-		// Find and print the minimum stores to visit
 		List<String> result = findMinimumStores(stores, requiredItems);
 		System.out.println("Visit the following stores: " + result);
 	}
